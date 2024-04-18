bff8001e848ad5c0af101976954c2761e376af54
AJAZ BHAT <ajazbhat2529@gmail.com>
2024-04-18 19:09:18 +0530

    add getusers

--- src/main/java/com/ums/controller/AuthController.java
+++ src/main/java/com/ums/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //@RequiredArgsConstructor
@@ -52,6 +53,12 @@ public class AuthController {
     public AppUser getCurrentProfile(@AuthenticationPrincipal AppUser user){
         return user;
     }
+    @GetMapping("/getuser")
+    public List<AppUser> getUsers(){
+
+        return new ArrayList<>();
+    }
+
 
 
 
